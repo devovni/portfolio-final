fd1dfb23e5197db5fb7001e13e66507affe88975
dumdud <ignaciogonzalez@gmail.com>
2024-11-28 13:16:26 -0300

    Updated landing-page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { SkillsCardComponent } from "../../components/landing/skills-card/skills-card.component";
 import { ProjectsCardComponent } from "../../components/landing/projects-card/projects-card.component";
-import { HeaderComponent } from "../../components/shared/header/header.component";
-import { PresentationCardComponent } from "../../components/landing/presentation-card/presentation-card.component";
+import { HeaderComponent } from "../../shared/header/header.component";
+
 import { AboutCardComponent } from "../../components/landing/about-card/about-card.component";
 import { ContactCardComponent } from "../../components/landing/contact-card/contact-card.component";
-import { FooterComponent } from "../../components/shared/footer/footer.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 import { HeroBannerComponent } from '../../components/landing/hero-banner/hero-banner.component';
+import { BackgroundComponent } from '../../shared/background/background.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [SkillsCardComponent, ProjectsCardComponent, HeaderComponent, PresentationCardComponent, AboutCardComponent, ContactCardComponent, FooterComponent, HeroBannerComponent],
+  imports: [SkillsCardComponent, ProjectsCardComponent, HeaderComponent, AboutCardComponent, ContactCardComponent, FooterComponent, HeroBannerComponent, BackgroundComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
