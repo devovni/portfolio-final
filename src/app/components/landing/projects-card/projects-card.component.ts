5cf618d2a9c4f15e34dcb43fa60fa8c9c01f7e9d
dumdud <ignaciogonzalez@gmail.com>
2024-11-28 21:40:26 -0300

    Updated project-card

--- src/app/components/landing/projects-card/projects-card.component.ts
+++ src/app/components/landing/projects-card/projects-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-projects-card',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './projects-card.component.css'
 })
 export class ProjectsCardComponent {
-
+items = [{
+  title: "test 1",
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea itaque
+                        necessitatibus
+                        deserunt repudiandae sint cupiditate, magnam ipsam ipsa provident in? Expedita, in distinctio.
+                        Reiciendis libero, maiores vero voluptas debitis soluta.`,
+  image: "https://placehold.co/600x400"
+},
+{
+  title: "test 2",
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea itaque
+                        necessitatibus
+                        deserunt repudiandae sint cupiditate, magnam ipsam ipsa provident in? Expedita, in distinctio.
+                        Reiciendis libero, maiores vero voluptas debitis soluta.`,
+  image: "https://placehold.co/600x400"
+}]
 }
