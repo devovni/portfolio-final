e0c1c4c5f9e0f703e3f6cb976834ee81dd7d7047
dumdud <ignaciogonzalez@gmail.com>
2024-12-02 14:31:49 -0300

    Added localization tags to corresponding html elements

--- src/app/components/landing/projects-card/projects-card.component.ts
+++ src/app/components/landing/projects-card/projects-card.component.ts
@@ -10,16 +10,16 @@ import { Title } from '@angular/platform-browser';
 })
 export class ProjectsCardComponent {
 items = [{
-  title: "test 1",
-  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea itaque
+  title: $localize `test 1`,
+  description: $localize `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea itaque
                         necessitatibus
                         deserunt repudiandae sint cupiditate, magnam ipsam ipsa provident in? Expedita, in distinctio.
                         Reiciendis libero, maiores vero voluptas debitis soluta.`,
   image: "https://placehold.co/600x400"
 },
 {
-  title: "test 2",
-  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea itaque
+  title: $localize `test 2`,
+  description: $localize `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea itaque
                         necessitatibus
                         deserunt repudiandae sint cupiditate, magnam ipsam ipsa provident in? Expedita, in distinctio.
                         Reiciendis libero, maiores vero voluptas debitis soluta.`,
