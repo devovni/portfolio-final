f4958f47620900ae01e65638413429e66243a92b
dumdud <ignaciogonzalez@gmail.com>
2024-12-06 03:02:17 -0300

    added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,24 @@ module.exports = {
   theme: {
     extend: {
       colors:{
+        Primary: "#eff1f5",
+        Secondary: "#ccd0da",
+        Accent: "#40a02b",
+        Typogragry: "#4c4f69",
+        darkPrimary: "#303446",
+        darkSecondary: "#232634",
         darkAccent: "#ef9f76",
         darkTypography: '#b5bfe2'
+      },
+      boxShadow:{
+        'button': ' 0px 4px 0px 0px rgba(0,0,0,1)',
+        'button-sm': ' 0px 2px 0px 0px rgba(0,0,0,1)'
+
+      },
+      fontFamily:{
+        'kanit':['kanit','mono'],
+        'onest':['Onest'],
+        'Roboto': 'Roboto'
       }
     },
   },
